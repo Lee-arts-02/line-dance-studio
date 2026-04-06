0f4bccad97bf3bf51a928e851c93701f5a114b6a
Lee-arts-02 <jiangli9811@outlook.com>
2026-04-06 14:27:32 -0400

    deploying body music project

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: 'export', 
+  images: {
+    unoptimized: true, 
+  },
   transpilePackages: [
     "@tensorflow/tfjs-core",
     "@tensorflow/tfjs-backend-webgl",
-    "@tensorflow-models/pose-detection",
+    "@tensorflow/models/pose-detection",
   ],
 };
 
